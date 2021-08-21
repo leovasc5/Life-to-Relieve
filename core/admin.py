7fcb5e77f2c0ace668889c902a071c7fba55ecb9
leovasc5 <leovasc5@hotmail.com>
2021-08-21 11:58:44 -0300

    Created tables and models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from core.models import Evento
 
-# Register your models here.
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'data_evento', 'data_criacao')
+
+
+admin.site.register(Evento, EventoAdmin)
