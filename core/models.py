5823bb7dfbc9641bdc7896102ae9b007079788bd
leovasc5 <leovasc5@hotmail.com>
2021-08-22 18:22:30 -0300

    Edited the Events table, added event_local to the attributes

--- core/models.py
+++ core/models.py
@@ -5,6 +5,7 @@ class Evento(models.Model):
     titulo = models.CharField(max_length=100)
     descricao = models.TextField(blank=True, null=True)
     data_evento = models.DateTimeField(verbose_name='Data do Evento')
+    local_evento = models.CharField(max_length=150, blank=True, null=True)
     data_criacao = models.DateTimeField(auto_now=True)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
 
--- core/views.py
+++ core/views.py
@@ -39,11 +39,13 @@ def submit_evento(request):
     if request.POST:
         titulo = request.POST.get('titulo')
         data_evento = request.POST.get('data_evento')
+        local_evento = request.POST.get('local_evento')
         descricao = request.POST.get('descricao')
         usuario = request.user
         Evento.objects.create(
             titulo=titulo,
             data_evento=data_evento,
+            local_evento=local_evento,
             descricao=descricao,
             usuario=usuario
         )
