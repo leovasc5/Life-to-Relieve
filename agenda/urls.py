66066ab9b0f9ccf0aafbcd018d32710d9b29c00b
leovasc5 <leovasc5@hotmail.com>
2021-08-21 22:57:44 -0300

    Created authentication system, login and decorator sessions

--- agenda/urls.py
+++ agenda/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('agenda/', views.lista_eventos),
     path('', RedirectView.as_view(url='/agenda/')),
+    path('login/', views.login_user),
+    path('login/submit', views.submit_login),
+    path('logout/', views.logout_user),
     path('eventos/<str:titulo_evento>/', views.local),
+
 ]
--- core/views.py
+++ core/views.py
@@ -1,12 +1,35 @@
 from django.shortcuts import render, HttpResponse, redirect
 from core.models import Evento
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def local(request, titulo_evento):
     objeto = Evento.objects.get(titulo=titulo_evento)
     return HttpResponse('<h1>Titulo: {}</h1>'.format(objeto.descricao))
 
+def login_user(request):
+    return render(request, 'login.html')
+
+@login_required(login_url='/login/')
 def lista_eventos(request):
     usuario = request.user
     evento = Evento.objects.filter(usuario=usuario)
     response = {'eventos': evento}
     return render(request, 'agenda.html', response)
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else:
+            messages.error(request, "Usuário ou senha inválido!")
+        return redirect('/')
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
