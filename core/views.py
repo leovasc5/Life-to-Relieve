41b4e5feb64568049a490f63b2bf3ecbc2a8a949
leovasc5 <leovasc5@hotmail.com>
2021-08-25 23:34:55 -0300

    Coded Responsive Design

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse, redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from core.models import Evento
 from django.contrib.auth.decorators import login_required
